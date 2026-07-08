fb1bfe8afd07c396017b4fcc565174051e8e1472
LingoJack <3065225677@qq.com>
2026-07-08 23:31:52 +0800

    更新: 2026-07-08 23:31:52

--- src/components/editor/sectionEditor/SectionOutline.tsx
+++ src/components/editor/sectionEditor/SectionOutline.tsx
@@ -248,7 +248,7 @@ export default function SectionOutline({
   }, []);
 
   return (
-    <div className="w-[240px] shrink-0 h-full border-l border-[var(--vscode-sideBar-border)] bg-[var(--vscode-sideBar-background)] flex flex-col select-none">
+    <div className="w-[240px] shrink-0 h-full border-l border-[var(--vscode-sideBar-border)] bg-[var(--vscode-sideBar-background)] flex flex-col pt-5 select-none">
       <div className="px-5 mb-5 shrink-0">
         <h2 className="text-sm font-semibold uppercase tracking-wide text-[var(--vscode-descriptionForeground)]">
           {t('outline.title')}
